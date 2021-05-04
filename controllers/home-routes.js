b3dbc0757c190e1a4496d74608fd134d4367db6f
adamalcantara <admajalcantara@icloud.com>
2021-05-04 14:01:31 -0400

    Added code to user-routes.js

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -6,19 +6,19 @@ router.get('/', async (req, res) => {
 });
 
 router.get('/dashboard', async (req, res) => {
-    res.render('maindash', {layout: "dashboard.handlebars"})
+    res.render('maindash', {layout: "dashboard.handlebars", loggedIn: req.session.loggedIn })
 });
 
 router.get('/profiles', async (req, res) => {
-    res.render('viewprofiles', {layout: "dashboard.handlebars"})
+    res.render('viewprofiles', {layout: "dashboard.handlebars", loggedIn: req.session.loggedIn })
 });
 
 router.get('/quiz', async (req, res) => {
-    res.render('quiz', {layout: "dashboard.handlebars"})
+    res.render('quiz', {layout: "dashboard.handlebars", loggedIn: req.session.loggedIn })
 });
 
 router.get('/settings', async (req, res) => {
-    res.render('quiz', {layout: "dashboard.handlebars"})
+    res.render('quiz', {layout: "dashboard.handlebars", loggedIn: req.session.loggedIn })
 });
 
 module.exports = router;
