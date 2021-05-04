189be85714f5389c73a02a5172e5bee9cb71ba7f
adamalcantara <admajalcantara@icloud.com>
2021-05-04 12:51:36 -0400

    Changed handlebars folder structure, updated routes

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -2,11 +2,15 @@ const router = require('express').Router();
 //require users
 
 router.get('/', async (req, res) => {
-    res.render('homepage')
+    res.render('homepage', {layout: "main.handlebars"})
 });
 
 router.get('/dashboard', async (req, res) => {
-    res.render('dashboard')
+    res.render('maindash', {layout: "dashboard.handlebars"})
+});
+
+router.get('/profiles', async (req, res) => {
+    res.render('viewprofiles', {layout: "dashboard.handlebars"})
 });
 
 module.exports = router;
