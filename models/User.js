061601eec6c556ffb183f0f576d022aee3b7e934
steveSnyder <74982034+stevesnyder@users.noreply.github.com>
2021-05-06 13:49:20 -0400

    Added Image Upload frontend and backend

--- models/User.js
+++ models/User.js
@@ -83,8 +83,6 @@ User.init(
       },
     },
 
-  },
-  {
     sequelize,
     timestamps: false,
     freezeTableName: true,
--- models/index.js
+++ models/index.js
@@ -6,24 +6,24 @@ const Movies = require('./Movies');
 const Image = require('./Image');
 
 User.hasOne(Preference, {
-    foreignKey: user_id,
+    foreignKey: 'user_id',
 });
 Preference.belongsTo(User, {
-    foreignKey: user_id,
+    foreignKey: 'user_id',
 });
 
 User.hasMany(Match, {
-    foreignKey: user_id,
+    foreignKey: 'user_id',
 });
 Match.belongsTo(User, {
-    foreignKey: user_id,
+    foreignKey: 'user_id',
 });
 
 User.hasMany(Image,  {
-    foreignKey: user_id,
+    foreignKey: 'user_id',
 });
 Image.belongsTo(User, {
-    foreignKey: user_id,
+    foreignKey: 'user_id',
 });
 
 // Need to add hasMany function for Movies
