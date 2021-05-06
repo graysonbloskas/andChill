498035c0fceac3cd371f5eb39691ccad93056c70
steveSnyder <74982034+stevesnyder@users.noreply.github.com>
2021-05-06 17:34:15 -0400

    added image-index file

---
+++ models/image-index.js
@@ -0,0 +1,17 @@
+const dbConfig = require("../config/connection.js");
+
+const Sequelize = require("sequelize");
+const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
+  host: dbConfig.HOST,
+  dialect: dbConfig.dialect,
+  operatorsAliases: false,
+});
+
+const db = {};
+
+db.Sequelize = Sequelize;
+db.sequelize = sequelize;
+
+db.images = require("./Image");
+
+module.exports = db;
