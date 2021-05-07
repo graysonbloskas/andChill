a1a389b095a4d72b66ce5192b1ad7f527f134b45
adamalcantara <admajalcantara@icloud.com>
2021-05-06 21:35:29 -0400

    Fixed routes

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,18 +1,18 @@
-// const userFetcher = async function getAllUsers(event) {
-//     event.preventdefault
-//     const users=await fetch("/api/users") 
-//     console.log(users.body)
-//     // document.location.reload()
-// }
+const userFetcher = async (event) => {
+    event.preventDefault();
+    const users=await fetch("/api/users/users") 
+    console.log(users)
+    // document.location.reload()
+}
 
 // const sequelize = require('../../config/connection')
-const getBtn = document.getElementById("get-matches") 
-const getUsers = () => {
-    connection.query('SELECT * FROM user', (err, data)=> {
-        if (err) throw err;
-        console.table(data)
-    })
-}
-getBtn.addEventListener("click", getUsers)
+// const getBtn = document.getElementById("get-matches") 
+// const getUsers = () => {
+//     connection.query('SELECT * FROM user', (err, data)=> {
+//         if (err) throw err;
+//         console.table(data)
+//     })
+// }
+// getBtn.addEventListener("click", getUsers)
 
-// document.querySelector("#get-matches").addEventListener("click", userFetcher)
+document.querySelector("#get-matches").addEventListener("click", userFetcher)
